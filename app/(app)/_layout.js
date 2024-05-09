fecc26e6eb91ecc5458ef44f9d229da5f19a15fc
RAZEX0LOL <khattayev00@mail.ru>
2024-05-10 03:25:17 +0400

    Create App and Setup Auth

---
+++ app/(app)/_layout.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View,Text} from "react-native";
+
+export default function _layout(){
+    return(
+        <View>
+            <Text>_layout</Text>
+        </View>
+    );
+}
